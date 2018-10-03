48034ad3a7d055adc64826b5e520d083f1f95e88
Fi <bmofa@pm.me>
2018-10-03 00:30:07 -0400

    Some more edits [32m# Please enter the commit message for your changes. Lines starting [32m# with '#' will be ignored, and an empty message aborts the commit. [32m# [32m# On branch master [32m# Your branch is up to date with 'origin/master'. [32m# [32m# Changes to be committed: [32m#       modified:   bashcord.sh [32m#

--- bashcord.sh
+++ bashcord.sh
@@ -1,12 +1,13 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # bashcord - shell script Discord API wrapper
-# despite the name, it *should* be all POSIX SH
+# despite the name, it *should* be all POSIX SH except for the usage of read
 
 ###########
 # Utility #
 ###########
 log() {
-	case "$1" in
+	if [ "${ENABLE_DEBUG}" = "true" ] || [ "${1}" = "DEBUG" ]; then return; fi
+	case "${1}" in
 		ERROR)
 			color=31
 			;;
@@ -27,9 +28,10 @@ log() {
 			;;
 	esac
 	if [ "${COLORED_LOGGING}" = "true" ]; then
-		echo "\033[1m$(date "+%y-%m-%d %H:%M:%S")\033[m \033[${color};1m$1\033[m - $2" >&2
+		echo "\033[1m$(date "+%y-%m-%d %H:%M:%S") \033[m\
+\033[${color};1m${1}\033[m - ${2}" >&2
 	else
-		echo "$(date "+%y-%m-%d %H:%M:%S") $1 - $2" >&2
+		echo "$(date "+%y-%m-%d %H:%M:%S") ${1} - ${2}" >&2
 	fi
 }
 alias error="log ERROR"
@@ -39,8 +41,8 @@ alias debug="log DEBUG"
 alias event="log EVENT"
 
 require() {
-	if ! [ -x "$(which $1)" ]; then
-		error "'$1' is required but not installed"
+	if ! [ -x "$(which "${1}")" ]; then
+		error "'${1}' is required but not installed"
 		exit 1
 	fi
 }
@@ -63,10 +65,10 @@ BOT_VERSION="1.0"
 #####################
 
 api() {
-	[ -z "$1" ]         && error "No method for request"      && exit 1
-	[ -z "$2" ]         && error "No endpoint for request"    && exit 2
-	[ -z "$BOT_AUTH" ]  && error "Authorization not defined"  && exit 3
-	[ -z "$BOT_AGENT" ] && error "Bot User-Agent not defined" && exit 4
+	[ -z "${1}" ]         && error "No method for request"      && exit 1
+	[ -z "${2}" ]         && error "No endpoint for request"    && exit 2
+	[ -z "${BOT_AUTH}" ]  && error "Authorization not defined"  && exit 3
+	[ -z "${BOT_AGENT}" ] && error "Bot User-Agent not defined" && exit 4
 	# If called with data to upload, add it to the curl request with `-d`
 	event "${1} to ${2} $([ -n "${3}" ] && echo "with ${3}")"
 	curl -X "${1}" -s -H "${BOT_AUTH}" -A "${BOT_AGENT}" "${API}${2}" $([ -n "${3}" ] && echo "-d ${3}")
@@ -82,14 +84,14 @@ _pre_init() {
 # Default post_init.
 #
 _post_init() {
-	echo "Connecting as... $(api GET /users/@me | jq .username -r)"
+	inform "Connecting as... $(api GET /users/@me | jq .username -r)"
 }
 
 # Setup some things and connect to the Discord API.
 #
 connect() {
 	# Run pre-init routines
-	if [ "$(type pre_init)" = "function" ]; then pre_init; else _pre_init; fi
+	if echo "$(type pre_init)" | grep -q "function"; then pre_init; else _pre_init; fi
 	[ -z "${API_TOKEN}" ] && error "API token not set" && exit
 	# Configure derivative variables
 	API_TOKEN="$(cat token)"
@@ -105,5 +107,5 @@ connect() {
 	BOT_AGENT="DiscordBot (${BOT_URL}, ${BOT_VERSION})"
 	debug "User-Agent: ${BOT_AGENT}"
 	# Run post-init routines
-	if [ "$(type post_init)" = "function" ]; then post_init; else _post_init; fi
+	if echo "$(type post_init)" | grep -q "function"; then post_init; else _post_init; fi
 }
