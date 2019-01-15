2f4b0ec5fdbb648b100d29ea2532e8a6e250a540
Fi <trvv@pm.me>
2019-01-15 14:34:40 -0500

    Works... kind of

--- lib/client.sh
+++ lib/client.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-source "bashcord.sh"
+source "util.sh"
+source "api.sh"
+
+api:init "https://discordapp.com/api" \
+         "Bot NDc1MTAyMjMyNDU1NTQ0ODM0.Dx-fhA.G478WL9Y81Ho80kdHe4JgwP5eL0" \
+         "https://github.com/trvv/bashcord" "1.0"
+
+api:me
+echo "Logged in as... ${ME[username]}"
+pmap ME
+
+api:get "/users/@me/guilds"
+pmap REPLY
